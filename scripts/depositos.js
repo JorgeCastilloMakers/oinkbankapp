4a1cc641a428d4edc2b5a9547bac4876158c4574
Jorge Castillo <103472285+JorgeCastilloMakers@users.noreply.github.com>
2022-08-07 23:43:53 -0300

    Add files via upload

--- scripts/depositos.js
+++ scripts/depositos.js
@@ -94,48 +94,43 @@ function depositarPropia(){
     document.getElementById("monto-deposito-propio").value="";
     divCtaPropia.style.display = "none";
 };
+}
+function depositarTerceros(){
 
-function depositarTerceros() {
     arrayDeUsers.forEach(elements=> {
         if(cbuTercero.value === elements.cbu) {
-            console.log("test primera condicion");
-            if(usuariONN.saldo >= montoDepositoTercero.value) {
+            if(usuariONN.saldo >= montoDepositoTercero.value && montoDepositoTercero.value > 0) {
                 elements.saldo = Number(elements.saldo) + Number(montoDepositoTercero.value); 
                 usuariONN.saldo = Number(usuariONN.saldo) - Number(montoDepositoTercero.value);
-                console.log("segunda condicion")
                 arrayDeUsers.forEach(e => {
                     if (e.cbu === usuariONN.cbu) {
                         e.saldo = e.saldo - montoDepositoTercero.value;
                     }
                     localStorage.setItem("userON", JSON.stringify(usuariONN));
                     localStorage.setItem("users", JSON.stringify(arrayDeUsers));
-                    console.log("test tercera condicion")
                     Swal.fire({
-                        position: 'center',
-                        icon: 'success',
-                        title: 'Su transferencia se realizó con éxito',
-                        showConfirmButton: true,
-                        timer: 3000,
+                        title: '¡Su deposito se realizo con éxito!',
+                        imageUrl: '/images/happy-pig.gif',
+                        imageWidth: 100,
+                        imageHeight: 100,
+                        imageAlt: 'happy-pig',
                         confirmButtonColor: '#FF9999',
-                        customClass:'sweet-fire',
-                    })
-                    console.log("cuarta")
+                      })
                 });
-            } else {
-                console.log("quinta - error");
-                Swal.fire({
-                    position: 'center',
-                    icon: 'error',
-                    title: 'Su transferencia no pudo ser procesada.',
-                    showConfirmButton: true,
-                    timer: 3000,
-                    confirmButtonColor: '#FF9999',
-                    customClass:'sweet-fire',
-                })
-            };
-        };
+            } 
+        }else {
+            Swal.fire({
+                title: 'Oops...!',
+                text: 'Su deposito no pudo ser procesado.',
+                imageUrl: '/images/cry-pig.png',
+                imageWidth: 100,
+                imageHeight: 100,
+                imageAlt: 'cry-pig',
+                confirmButtonColor: '#FF9999',
+              })
+        };;
     });
     document.getElementById("monto-deposito-tercero").value="";
-    divCtaTercero.style.display = "none";    
-};
+    divCtaTercero.style.display = "none";   
 }
+
